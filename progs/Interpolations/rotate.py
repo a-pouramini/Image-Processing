7594a9154a31fd707784b89c5e5f1f089ce50160
Ahmad Pouramini <pouramini@gmail.com>
2024-10-08 11:36:25 +0330

    commit

--- progs/Interpolations/rotate.py
+++ progs/Interpolations/rotate.py
@@ -30,48 +30,22 @@ M[1, 2] += bound_h / 2 - center[1]
 # Apply the affine transformation (rotation)
 rotated = cv2.warpAffine(img, M, (bound_w, bound_h))
 
-# Scaling
-scale_percent = 110
-width = int(rotated.shape[1] * scale_percent / 100)
-height = int(rotated.shape[0] * scale_percent / 100)
-dim = (width, height)
-
-# Resize using different interpolation methods
-scaled = cv2.resize(rotated, dim, interpolation=cv2.INTER_NEAREST)
-scaled1 = cv2.resize(rotated, dim, interpolation=cv2.INTER_LINEAR)
-scaled2 = cv2.resize(rotated, dim, interpolation=cv2.INTER_CUBIC)
-
 # Convert grayscale images to RGB for matplotlib display
+img_rgb = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 rotated_rgb = cv2.cvtColor(rotated, cv2.COLOR_GRAY2RGB)
-scaled_rgb = cv2.cvtColor(scaled, cv2.COLOR_GRAY2RGB)
-scaled1_rgb = cv2.cvtColor(scaled1, cv2.COLOR_GRAY2RGB)
-scaled2_rgb = cv2.cvtColor(scaled2, cv2.COLOR_GRAY2RGB)
-
 # Display the images using matplotlib
 plt.figure(figsize=(15, 10))
 
 # Rotated Image
-plt.subplot(2, 3, 1)
-plt.imshow(rotated_rgb)
-plt.title(f"Rotated by {angle} Degrees")
+plt.subplot(1, 2, 1)
+plt.imshow(img_rgb)
+plt.title("Original Image")
 plt.axis('off')
 
 # Nearest Neighbor Scaling
-plt.subplot(2, 3, 2)
-plt.imshow(scaled_rgb)
-plt.title("Nearest Neighbor Scaling")
-plt.axis('off')
-
-# Bilinear Scaling
-plt.subplot(2, 3, 4)
-plt.imshow(scaled1_rgb)
-plt.title("Bilinear Scaling")
-plt.axis('off')
-
-# Bicubic Scaling
-plt.subplot(2, 3, 5)
-plt.imshow(scaled2_rgb)
-plt.title("Bicubic Scaling")
+plt.subplot(1, 2, 2)
+plt.imshow(rotated_rgb)
+plt.title(f"Rotated by {angle} Degrees")
 plt.axis('off')
 
 # Show all images
